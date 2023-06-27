74e3c83f91689df1b60a4821a962c7f27f56dd05
Arnab Hazra <arnhazra@gmail.com>
2023-06-27 23:25:36 +0530

    ProdDeploy

--- api/handler.ts
+++ api/handler.ts
@@ -8,11 +8,13 @@ import UserRouter from './src/routes/UserRouter'
 import TransactionRouter from './src/routes/TransactionRouter'
 import EvolakeRouter from './src/routes/EvolakeRouter'
 import AirlakeRouter from './src/routes/AirlakeRouter'
+import IcelakeRouter from './src/routes/IcelakeRouter'
 
 const userRouter = new UserRouter()
 const transactionRouter = new TransactionRouter()
 const airlakeRouter = new AirlakeRouter()
 const evolakeRouter = new EvolakeRouter()
+const icelakeRouter = new IcelakeRouter()
 
 const app = express()
 app.listen(envConfig.apiPort)
@@ -25,6 +27,7 @@ app.use('/api/user', userRouter.getRouter())
 app.use('/api/transaction', transactionRouter.getRouter())
 app.use('/api/airlake', airlakeRouter.getRouter())
 app.use('/api/evolake', evolakeRouter.getRouter())
+app.use('/api/icelake', icelakeRouter.getRouter())
 
 if (envConfig.nodeEnv === 'production') {
     const cacheControl = 'public, max-age=31536000'
