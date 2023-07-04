293f635620dff389b5b65145b473d4a54946d51c
Arnab Hazra <arnhazra@gmail.com>
2023-07-04 10:01:47 +0530

    ProdDeploy

--- client/src/utils/withoutAuth.tsx
+++ client/src/utils/withoutAuth.tsx
@@ -1,18 +1,14 @@
-import Loading from '@/components/Loading'
-import { useContext, useEffect, useState } from 'react'
-import axios from 'axios'
-import endPoints from '@/constants/apiEndpoints'
-import toast from 'react-hot-toast'
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { AppContext } from '@/context/appStateProvider'
-import Constants from '@/constants/appConstants'
 
 export default function withoutAuth<T>(WrappedComponent: React.ComponentType<T>) {
     return function WithoutAuth(props: any) {
         const router = useRouter()
 
         useEffect(() => {
-            router.push('/dashboard')
+            if (localStorage.getItem('accessToken')) {
+                router.push('/dashboard')
+            }
         }, [router.pathname])
 
         return <WrappedComponent {...props} />
