f8494e98c2605bae12f946381fc025112ef92fe1
Arnab Hazra <arnhazra@gmail.com>
2023-07-21 14:30:38 +0530

    ProdDeploy

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    images: {
-        unoptimized: true
-    },
-    output: 'export',
-    trailingSlash: true
 }
 
 module.exports = nextConfig
