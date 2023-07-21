4587c45140ca876fec77d5191c9c2249a9736c51
Arnab Hazra <arnhazra@gmail.com>
2023-07-21 14:26:08 +0530

    ProdDeploy

--- api/main.ts
+++ api/main.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
-import path from 'path'
 import { envConfig } from './config/envConfig'
 import { dbConnect } from './src/utils/dbConnect'
 import { connectRedis } from './src/utils/redisHelper'
@@ -12,42 +11,29 @@ import IcelakeRouter from './src/app/products/icelake/IcelakeRouter'
 import CommonRouter from './src/app/common/CommonRouter'
 import FrostlakeRouter from './src/app/products/frostlake/FrostlakeRouter'
 
-const userRouter = new UserRouter()
-const transactionRouter = new TransactionRouter()
-const airlakeRouter = new AirlakeRouter()
-const evolakeRouter = new EvolakeRouter()
-const icelakeRouter = new IcelakeRouter()
-const frostlakeRouter = new FrostlakeRouter()
-const commonRouter = new CommonRouter()
+export const bootstrap = () => {
+    const app = express()
+    app.listen(envConfig.apiPort)
+    app.use(cors())
+    app.use(express.json({ limit: '3mb' }))
+    dbConnect()
+    connectRedis()
 
-const app = express()
-app.listen(envConfig.apiPort)
-app.use(cors())
-app.use(express.json({ limit: '3mb' }))
-dbConnect()
-connectRedis()
+    const userRouter = new UserRouter()
+    const transactionRouter = new TransactionRouter()
+    const airlakeRouter = new AirlakeRouter()
+    const evolakeRouter = new EvolakeRouter()
+    const icelakeRouter = new IcelakeRouter()
+    const frostlakeRouter = new FrostlakeRouter()
+    const commonRouter = new CommonRouter()
 
-app.use('/api/user', userRouter.getRouter())
-app.use('/api/transaction', transactionRouter.getRouter())
-app.use('/api/common', commonRouter.getRouter())
-app.use('/api/products/airlake', airlakeRouter.getRouter())
-app.use('/api/products/evolake', evolakeRouter.getRouter())
-app.use('/api/products/icelake', icelakeRouter.getRouter())
-app.use('/api/products/frostlake', frostlakeRouter.getRouter())
-
-
-if (envConfig.nodeEnv === 'production') {
-    const cacheControl = 'public, max-age=31536000'
-    function setCustomCacheControl(res: Response, path: string) {
-        if (express.static.mime.lookup(path) === 'text/html') {
-            res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
-        } else {
-            res.setHeader('Cache-Control', cacheControl)
-        }
-    }
-
-    app.use(express.static(path.join(__dirname, 'client'), { maxAge: 60000, setHeaders: setCustomCacheControl }))
-    app.get('/*', (req: Request, res: Response) => {
-        res.sendFile(path.join(__dirname, 'client', `${req.originalUrl.split('?')[0]}.html`))
-    })
+    app.use('/api/user', userRouter.getRouter())
+    app.use('/api/transaction', transactionRouter.getRouter())
+    app.use('/api/common', commonRouter.getRouter())
+    app.use('/api/products/airlake', airlakeRouter.getRouter())
+    app.use('/api/products/evolake', evolakeRouter.getRouter())
+    app.use('/api/products/icelake', icelakeRouter.getRouter())
+    app.use('/api/products/frostlake', frostlakeRouter.getRouter())
 }
+
+bootstrap()
