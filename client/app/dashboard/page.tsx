435cd5792ee4d6ac919d2082d4a12af2a6d8b28e
Arnab Hazra <arnhazra@gmail.com>
2023-09-20 15:56:14 +0530

    ProdDeploy

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import useFetch from "@/_hooks/useFetch"
 import endPoints from "@/_constants/apiEndpoints"
 import HTTPMethods from "@/_constants/httpMethods"
 import AppCard from "@/_components/AppCard"
-import { Fragment, useContext } from "react"
+import { Fragment, useContext, useState } from "react"
 import Show from "@/_components/Show"
 import Loading from "@/_components/Loading"
 import { Button, Container, Row } from "react-bootstrap"
@@ -17,6 +17,7 @@ import Constants from "@/_constants/appConstants"
 const DashboardPage: NextPage = () => {
     const apps = useFetch("get-apps", endPoints.getPlatformConfigEndpoint, HTTPMethods.POST)
     const [{ userState }] = useContext(AppContext)
+    const [displayTrialButton, setDisplayTrialButton] = useState(userState.trialAvailable)
 
     const appsToDisplay = apps?.data?.map((app: any) => {
         return <AppCard key={app.appName} appName={app.appName} url={app.url} appAvailable={app.appAvailable} description={app.description} dbRegion={app.dbRegion} />
@@ -25,6 +26,7 @@ const DashboardPage: NextPage = () => {
     const activateTrial = async () => {
         try {
             await axios.post(endPoints.activateTrialEndpoint)
+            setDisplayTrialButton(false)
             toast.success(Constants.ToastSuccess)
         } catch (error) {
             toast.error(Constants.ToastError)
@@ -36,7 +38,7 @@ const DashboardPage: NextPage = () => {
             <Show when={!apps.isLoading}>
                 <Container>
                     <h4 className="dashboard-header">Welcome to Lenstack!</h4>
-                    <Show when={userState.trialAvailable}>
+                    <Show when={displayTrialButton}>
                         <Button onClick={activateTrial}>Activate Trial</Button>
                     </Show>
                     <Row className="mb-4">
