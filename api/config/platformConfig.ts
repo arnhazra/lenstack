db79205e29d80631ffe7c878944a10a61e93ce00
Arnab Hazra <arnhazra@gmail.com>
2023-09-20 14:14:08 +0530

    ProdDeploy

--- api/config/platformConfig.ts
+++ api/config/platformConfig.ts
@@ -1,43 +1,43 @@
 export const platformConfig = [
     {
         productName: "airlake",
-        description: "An open-source application for easily discovering, subscribing to, and utilizing third-party cloud data.",
-        url: "/airlake",
+        description: "An application for easily searching, subscribing to, and utilizing third-party cloud data into your application.",
+        url: "/products?productName=airlake",
         productAvailable: true,
         dbRegion: "MongoDB AWS AP-South-1"
     },
     {
         productName: "icelake",
-        description: "An open-souce application that offers users a secure scalable and economic storage solution for files and data.",
-        url: "/icelake",
+        description: "An application that offers users a secure scalable and economic storage solution for files, documents and data.",
+        url: "/products?productName=icelake",
         productAvailable: true,
         dbRegion: "MongoDB GCP AP-South-1"
     },
     {
         productName: "wealthnow",
-        description: "An open-source wealth management platform to manage personal and businees wealth in a consolidated dashboard.",
-        url: "/wealthnow",
+        description: "An wealth management platform to manage personal and businees wealth in a single dashboard with many features.",
+        url: "/products?productName=wealthnow",
         productAvailable: true,
         dbRegion: "MongoDB Azure CIN"
     },
     {
         productName: "evolake",
-        description: "An open-source application that utilizes the OpenAI Davinci model to generate database queries.",
-        url: "/evolake",
+        description: "An application that utilizes the OpenAI Davinci model to intelligently generate database queries based on input.",
+        url: "/products?productName=evolake",
         productAvailable: true,
         dbRegion: "MongoDB AWS AP-South-1"
     },
     {
         productName: "snowlake",
-        description: "An open-source prototype management application that uses Ethereum Blockchain to manages prototypes.",
-        url: "/snowlake",
+        description: "A prototype management application that uses Ethereum Blockchain to create and manage prototypes.",
+        url: "/products?productName=snowlake",
         productAvailable: true,
         dbRegion: "Polygon Testnet Mumbai"
     },
     {
         productName: "frostlake",
-        description: "An open-source analytics provider for web applications. It empowers you to make informed analysis.",
-        url: "/frostlake",
+        description: "An analytics provider for web applications. It empowers you to make informed analysis and better decisions.",
+        url: "/products?productName=frostlake",
         productAvailable: true,
         dbRegion: "MongoDB Azure CIN"
     },
---
+++ client/app/products/page.tsx
@@ -0,0 +1,47 @@
+"use client"
+import ProductCard from "@/_components/ProductCard"
+import endPoints from "@/_constants/apiEndpoints"
+import HTTPMethods from "@/_constants/httpMethods"
+import useFetch from "@/_hooks/useFetch"
+import { ArrowRightIcon } from "@radix-ui/react-icons"
+import { NextPage } from "next"
+import { useRouter, useSearchParams } from "next/navigation"
+import React from 'react'
+import { Button, Container, Row } from "react-bootstrap"
+
+const ProductPage: NextPage = () => {
+    const products = useFetch("get-products", endPoints.getPlatformConfigEndpoint, HTTPMethods.POST)
+    const searchParams = useSearchParams()
+    const router = useRouter()
+    const productName = searchParams.get("productName")
+
+    const selectedProduct = products?.data?.find((product: any) => {
+        return product.productName === productName
+    })
+
+    const productsToDisplay = products?.data?.filter((product: any) => product.productName !== productName).map((product: any) => {
+        return <ProductCard key={product.productName} productName={product.productName} url={product.url} productAvailable={product.productAvailable} description={product.description} dbRegion={product.dbRegion} />
+    })
+
+    const launchProduct = () => {
+        router.push(`/${productName}`)
+    }
+
+    return (
+        <Container>
+            <div className="jumbotron p-4">
+                <p className="branding text-capitalize">{selectedProduct?.productName}</p>
+                <p className="lead mt-3">{selectedProduct?.description}</p>
+                <Button className="tag-chip">{selectedProduct?.dbRegion}</Button>
+                <Button className="tag-chip">{selectedProduct?.productAvailable ? 'Available' : 'Under Maintainance'}</Button><br />
+                <Button className="mt-2" onClick={launchProduct}>Go to App<ArrowRightIcon className="icon-right" /></Button>
+            </div>
+            <h4 className="dashboard-header mt-2">Other Products</h4>
+            <Row className="mb-4 mt-2">
+                {productsToDisplay}
+            </Row>
+        </Container>
+    )
+}
+
+export default ProductPage
