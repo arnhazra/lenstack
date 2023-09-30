dbe550f268b8a95e507c6184ed24e22c5310c6ea
Arnab Hazra <arnhazra@gmail.com>
2023-09-30 10:51:58 +0530

    ProdDeploy

--- server/src/api/user/dto/request-auth-code.dto.ts
+++
@@ -1,3 +0,0 @@
-export class RequestAuthCodeDto {
-    readonly email: string
-}
--- server/src/api/user/dto/verify-auth-code.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types"
-import { RequestAuthCodeDto } from "./request-auth-code.dto"
-
-export class VerifyAuthCodeDto extends PartialType(RequestAuthCodeDto) {
-    readonly authCode: string
-    readonly hash: string
-    readonly privateKey: string
-}
