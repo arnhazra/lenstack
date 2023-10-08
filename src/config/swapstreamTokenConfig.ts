ed3cea4f54c8e5640ca451098ff4b229aa92ab2f
Arnab Hazra <arnhazra@gmail.com>
2023-10-08 08:45:08 +0530

    ProdDeploy

---
+++ src/config/swapstreamTokenConfig.ts
@@ -0,0 +1,44 @@
+export const swapstreamTokenConfig = [
+  {
+    "tokenName": "Bronze Token",
+    "tokenSymbol": "BT",
+    "tokenContractAddress": "0x87661286c067285BC0534a282e1CB341ab53482a",
+    "vendorContractAddress": "0xCBCF608ED581366C0dC420Ff5cD5878F765CcF38",
+    "tokensPerMatic": "100000"
+  },
+  {
+    "tokenName": "Silver Token",
+    "tokenSymbol": "ST",
+    "tokenContractAddress": "0x6aada0f320D21c0Ac209B004900B41aC30bf4786",
+    "vendorContractAddress": "0x714147640E37F8910456D7C31cc8Fb1187722702",
+    "tokensPerMatic": "50000"
+  },
+  {
+    "tokenName": "Gold Token",
+    "tokenSymbol": "GT",
+    "tokenContractAddress": "",
+    "vendorContractAddress": "",
+    "tokensPerMatic": "10000"
+  },
+  {
+    "tokenName": "Ruby Token",
+    "tokenSymbol": "RT",
+    "tokenContractAddress": "",
+    "vendorContractAddress": "",
+    "tokensPerMatic": "5000"
+  },
+  {
+    "tokenName": "Emerald Token",
+    "tokenSymbol": "ET",
+    "tokenContractAddress": "",
+    "vendorContractAddress": "",
+    "tokensPerMatic": "3000"
+  },
+  {
+    "tokenName": "Diamond Token",
+    "tokenSymbol": "DT",
+    "tokenContractAddress": "",
+    "vendorContractAddress": "",
+    "tokensPerMatic": "2500"
+  }
+]
