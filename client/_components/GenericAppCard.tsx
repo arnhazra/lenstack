4de140d8fefcb6b995b4681bf0e39e10ad607a7e
Arnab Hazra <arnhazra@gmail.com>
2023-11-10 13:44:45 +0530

    ProdDeploy

--- client/_components/GenericAppCard.tsx
+++ client/_components/GenericAppCard.tsx
@@ -11,13 +11,13 @@ export default function GenericAppCard({ genericAppCardProps }: GenericAppCardPr
   }
 
   return (
-    <Col xs={12} sm={12} md={6} lg={4} xl={3} className="mb-4">
+    <Col xs={12} sm={6} md={6} lg={4} xl={3} className="mb-4">
       <Card onClick={redirectToApp} className={`app-card-${genericAppCardProps.className}`}>
         <Badge bg="dark" pill className="position-absolute mt-2 me-2 top-0 end-0 ps-3 pe-3 p-2">{genericAppCardProps.badgeText}</Badge>
         <Card.Header className="pb-2 ps-4 app-card-header">
           <p className="branding app-name pb-2 pt-2">{genericAppCardProps.headerText}</p>
         </Card.Header>
-        <Card.Footer className="pt-4 ps-4 app-card-footer">
+        <Card.Footer className="pt-4 ps-4 app-card-footer mb-2">
           <div className="d-flex justify-content-between align-items-center">
             <p className="smalltext">{genericAppCardProps.footerText}</p>
           </div>
