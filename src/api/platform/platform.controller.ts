d3f61f95336e1c78b1f03a4aba4784c89e1ac0cd
Arnab Hazra <arnhazra@gmail.com>
2023-11-16 13:18:47 +0530

    ProdDeploy

--- src/api/platform/platform.controller.ts
+++ src/api/platform/platform.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, Post, Body } from "@nestjs/common"
 import { PlatformService } from "./platform.service"
 import { TokenAuthorizer, TokenAuthorizerResponse } from "src/authorization/tokenauthorizer/tokenauthorizer.decorator"
+import { ProductSearchDto } from "./dto/product-search.dto"
 
 @Controller("platform")
 export class PlatformController {
   constructor(private readonly platformService: PlatformService) { }
 
   @Post("getproductconfig")
-  async getProductConfig(@TokenAuthorizer() uft: TokenAuthorizerResponse, @Body("searchQuery") searchQuery: string) {
+  async getProductConfig(@TokenAuthorizer() uft: TokenAuthorizerResponse, @Body() productSearchDto: ProductSearchDto) {
     try {
+      const { searchQuery } = productSearchDto
       const products = await this.platformService.getProductConfig(searchQuery)
       return products
     }
