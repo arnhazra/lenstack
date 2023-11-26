0343796b52c1b81d5a48c3777bf70140ca62023f
Arnab Hazra <arnhazra@gmail.com>
2023-11-26 17:37:54 +0530

    ProdDeploy

--- client/src/app/products/layout.tsx
+++ client/src/app/products/layout.tsx
@@ -1,16 +1,19 @@
 "use client"
 import Show from "@/components/show.component"
+import { GlobalContext } from "@/context/globalstate.provider"
 import { BookmarkIcon, CubeIcon } from "@radix-ui/react-icons"
 import Link from "next/link"
-import { Fragment, ReactNode } from "react"
+import { Fragment, ReactNode, useContext } from "react"
 
 export default function ProductLayout({ children }: { children: ReactNode }) {
+  const [{ userState }] = useContext(GlobalContext)
+
   return (
     <Fragment>
-      <Show when={!!localStorage.getItem("clientId")}>
+      <Show when={!!userState.clientId}>
         {children}
       </Show>
-      <Show when={!localStorage.getItem("clientId")}>
+      <Show when={!userState.clientId}>
         <div className="box">
           <p className="branding">Hold On</p>
           <p className="muted-text">Seems like you are not having an active subscription in this workspace to use/view this Product</p>
