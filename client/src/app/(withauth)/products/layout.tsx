e0974aa24f1bcae5ac2b429cf486b6a3d447c7cf
Arnab Hazra <arnhazra@gmail.com>
2023-12-14 22:23:19 +0530

    ProdDeploy

--- client/src/app/(withauth)/products/layout.tsx
+++ client/src/app/(withauth)/products/layout.tsx
@@ -1,16 +1,34 @@
 "use client"
 import Show from "@/components/show-component"
+import SubHeader from "@/components/sub-header-component"
 import { GlobalContext } from "@/context/globalstate.provider"
-import { BookmarkIcon, CubeIcon } from "@radix-ui/react-icons"
+import { BookmarkIcon, CubeIcon, HomeIcon } from "@radix-ui/react-icons"
 import Link from "next/link"
+import { usePathname, useRouter } from "next/navigation"
 import { Fragment, ReactNode, useContext } from "react"
 
 export default function ProductLayout({ children }: { children: ReactNode }) {
   const [{ userState }] = useContext(GlobalContext)
+  const pathname = usePathname()
+  const router = useRouter()
+  const urlParts = pathname.split("/")
+  const productsIndex = urlParts.indexOf('products')
+  let productName = ""
+  if (productsIndex !== -1 && productsIndex + 1 < urlParts.length) {
+    productName = urlParts[productsIndex + 1]
+  }
 
   return (
     <Fragment>
       <Show when={userState.hasActiveSubscription}>
+        <SubHeader>
+          <div className="d-flex justify-content-between align-items-center">
+            <h4 className="text-white">Lenstack {productName}</h4>
+            <div className="ml-auto">
+              <HomeIcon className="icon-subheader" onClick={() => router.push(`/products/${productName}`)} />
+            </div>
+          </div>
+        </SubHeader>
         {children}
       </Show>
       <Show when={!userState.hasActiveSubscription}>
