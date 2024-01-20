dc29ebb1172773986301fa59a76d7db1c52cf213
Arnab Hazra <arnhazra@gmail.com>
2024-01-20 20:56:37 +0530

    publish

--- src/api/products/copilot/commands/generate-response.command.ts
+++ src/api/products/copilot/commands/generate-response.command.ts
@@ -1,3 +1,17 @@
+import { GoogleGenerativeAI } from "@google/generative-ai"
+import { envConfig } from "src/env.config"
+
 export async function generateResponse(prompt: string) {
-  return prompt
+  try {
+    const genAI = new GoogleGenerativeAI(envConfig.geminiAPIKey)
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" })
+    const result = await model.generateContent(prompt)
+    const response = result.response
+    const textResponse = response.text()
+    return textResponse
+  }
+
+  catch (error) {
+    throw error
+  }
 }
