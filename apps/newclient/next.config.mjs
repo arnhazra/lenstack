85b5fb6eba1adff43bd186c7ea2ad4783c668eb6
Arnab Hazra <arnhazra@gmail.com>
2024-01-26 10:06:01 +0530

    publish

--- apps/newclient/next.config.mjs
+++ apps/newclient/next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    unoptimized: true
+  },
+  output: "export",
+  trailingSlash: process.env.NODE_ENV === "production" ? true : false,
+  experimental: {
+    missingSuspenseWithCSRBailout: false
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
