54c384c2a495f8fb11bbfb96493b460b55c00767
Arnab Hazra <arnhazra@gmail.com>
2024-01-31 21:38:31 +0530

    publish

--- apps/ui/src/context/providers/style.provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import { Fragment, ReactNode } from "react"
-import "@/styles/globals.style.sass"
-
-export default function StyleProvider({ children }: { children: ReactNode }) {
-  return (
-    <Fragment>{children}</Fragment>
-  )
-}
