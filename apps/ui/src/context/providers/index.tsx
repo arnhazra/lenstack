7a109ece14dd3ea8bffcd83a9acaf0305685cbf8
Arnab Hazra <arnhazra@gmail.com>
2024-02-01 15:14:42 +0530

    publish

--- apps/ui/src/context/providers/index.tsx
+++ apps/ui/src/context/providers/index.tsx
@@ -6,23 +6,20 @@ import { GlobalStateProvider } from "./globalstate.provider"
 import { ConfirmProvider } from "./confirm.provider"
 import { PromptProvider } from "./prompt.provider"
 import { Toaster } from "react-hot-toast"
-import OfflineProvider from "./offline.provider"
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <StyleProvider>
-      <OfflineProvider>
-        <ReactQueryProvider>
-          <GlobalStateProvider>
-            <ConfirmProvider>
-              <PromptProvider>
-                {children}
-                <Toaster position="bottom-right" />
-              </PromptProvider>
-            </ConfirmProvider>
-          </GlobalStateProvider>
-        </ReactQueryProvider>
-      </OfflineProvider>
+      <ReactQueryProvider>
+        <GlobalStateProvider>
+          <ConfirmProvider>
+            <PromptProvider>
+              {children}
+              <Toaster position="bottom-right" />
+            </PromptProvider>
+          </ConfirmProvider>
+        </GlobalStateProvider>
+      </ReactQueryProvider>
     </StyleProvider>
   )
 }
