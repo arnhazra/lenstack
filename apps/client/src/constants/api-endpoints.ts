670b0e8c7e41f3e644c93277eab933a5e6d84efd
Arnab Hazra <arnhazra@gmail.com>
2024-02-08 11:45:24 +0530

    publish

--- apps/client/src/constants/api-endpoints.ts
+++ apps/client/src/constants/api-endpoints.ts
@@ -1,7 +1,7 @@
 import { uiConstants } from "./global-constants"
 
-export const apiHost = process.env.NODE_ENV === "development" ? "http://localhost:8000" : `https://${uiConstants.brandName}.vercel.app`
-export const uiHost = process.env.NODE_ENV === "development" ? "http://localhost:3000" : `https://${uiConstants.brandName}.vercel.app`
+export const apiHost = process.env.NODE_ENV === "development" ? "http://localhost:8000" : "/"
+export const uiHost = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/"
 
 export const endPoints = {
   getapireference: `${apiHost}/api/apireference/get`,
