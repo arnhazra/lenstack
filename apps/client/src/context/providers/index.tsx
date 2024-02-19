bbb01571a57402d8a105675af795bd02a11a2e03
Arnab Hazra <arnhazra@gmail.com>
2024-02-19 09:07:11 +0530

    publish

--- apps/client/src/context/providers/index.tsx
+++ apps/client/src/context/providers/index.tsx
@@ -5,21 +5,24 @@ import ReactQueryProvider from "./reactquery.provider"
 import { GlobalStateProvider } from "./globalstate.provider"
 import { ConfirmProvider } from "./confirm.provider"
 import { PromptProvider } from "./prompt.provider"
+import { CachebustingProvider } from "./cachebusting.provider"
 import { Toaster } from "react-hot-toast"
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <StyleProvider>
-      <ReactQueryProvider>
-        <GlobalStateProvider>
-          <ConfirmProvider>
-            <PromptProvider>
-              {children}
-              <Toaster position="bottom-right" />
-            </PromptProvider>
-          </ConfirmProvider>
-        </GlobalStateProvider>
-      </ReactQueryProvider>
-    </StyleProvider>
+    <CachebustingProvider>
+      <StyleProvider>
+        <ReactQueryProvider>
+          <GlobalStateProvider>
+            <ConfirmProvider>
+              <PromptProvider>
+                {children}
+                <Toaster position="bottom-right" />
+              </PromptProvider>
+            </ConfirmProvider>
+          </GlobalStateProvider>
+        </ReactQueryProvider>
+      </StyleProvider>
+    </CachebustingProvider>
   )
 }
