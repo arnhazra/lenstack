c8b9300967cd67b793238b96d85b026e3228f2cc
Arnab Hazra <arnhazra@gmail.com>
2024-02-28 08:53:08 +0530

    publish

--- apps/client/src/context/providers/cachebusting.provider.tsx
+++ apps/client/src/context/providers/cachebusting.provider.tsx
@@ -6,7 +6,7 @@ import Loading from "@/components/loading"
 export function CachebustingProvider({ children }: { children: ReactNode }) {
   return (
     <CacheBuster
-      currentVersion="9.3.0"
+      currentVersion="9.3.1"
       isEnabled={process.env.NODE_ENV === "production"}
       isVerboseMode={false}
       loadingComponent={<Loading />}
