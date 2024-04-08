947f3b40c5d43198b95603757f6349ce515ce99d
Arnab Hazra <arnhazra@gmail.com>
2024-04-08 23:30:35 +0530

    publish

--- apps/web/src/app/(auth)/dashboard/page.tsx
+++ apps/web/src/app/(auth)/dashboard/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 import Link from "next/link"
-import { ArrowUpRight, BarChart2, Calendar, CalendarCheck2Icon, OrbitIcon } from "lucide-react"
+import { ArrowUpRight, BarChart2, Calendar, CalendarCheck2Icon, CheckCircle, OrbitIcon } from "lucide-react"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { Avatar } from "@/components/ui/avatar"
 import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { format } from "date-fns"
 import { GlobalContext } from "@/context/providers/globalstate.provider"
 import useQuery from "@/hooks/use-query"
@@ -15,15 +15,18 @@ import { endPoints } from "@/constants/api-endpoints"
 import HTTPMethods from "@/constants/http-methods"
 import { useRouter } from "next/navigation"
 import { uiConstants } from "@/constants/global-constants"
+import Suspense from "@/components/suspense"
 
 export default function Page() {
   const [{ userState }] = useContext(GlobalContext)
+  const [queryId, setQueryId] = useState("DQID")
   const products = useQuery(["products"], `${endPoints.getProductConfig}?searchQuery=`, HTTPMethods.GET)
   const pricingDetails = useQuery(["pricing"], endPoints.getSubscriptionConfig, HTTPMethods.GET)
+  const myWorkspaces = useQuery(["workspaces", queryId], endPoints.findMyWorkspaces, HTTPMethods.GET)
   const currentPlan = pricingDetails?.data?.find((plan: any) => plan.planName === userState.selectedPlan)
   const router = useRouter()
 
-  const productsToDisplay = products?.data?.slice(0, 3).map((product: any) => {
+  const renderProducts = products?.data?.slice(0, 3).map((product: any) => {
     return (
       <TableRow className="cursor-pointer" key={product.displayName} onClick={(): void => router.push(`/products/${product.productName}`)}>
         <TableCell>
@@ -34,6 +37,31 @@ export default function Page() {
         </TableCell>
         <TableCell className="text-right"><Badge variant="outline">{product?.productCategory}</Badge></TableCell>
       </TableRow>
+    )
+  })
+
+  const renderWorkspaces = myWorkspaces?.data?.myWorkspaces?.map((workspace: any) => {
+    return (
+      <div className="flex items-center gap-4" key={workspace._id}>
+        <Avatar className="hidden h-9 w-9 sm:flex">
+          <AvatarFallback>WS</AvatarFallback>
+        </Avatar>
+        <div className="grid gap-1">
+          <p className="text-sm font-medium leading-none">
+            {workspace.name}
+          </p>
+          <p className="text-sm text-muted-foreground">
+            Hello
+          </p>
+        </div>
+        <div className="ml-auto font-medium">
+          <Button variant="outline" disabled={userState.selectedWorkspaceId === workspace._id} className="btn-block" onClick={(): Promise<void> => switchWorkspace(workspace._id)}>
+            <Suspense condition={userState.selectedWorkspaceId !== workspace._id} fallback={<>Selected</>}>
+              Switch
+            </Suspense>
+          </Button>
+        </div>
+      </div>
     )
   })
 
@@ -119,7 +147,7 @@ export default function Page() {
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  {productsToDisplay}
+                  {renderProducts}
                 </TableBody>
               </Table>
             </CardContent>
@@ -129,81 +157,7 @@ export default function Page() {
               <CardTitle>Workspaces</CardTitle>
             </CardHeader>
             <CardContent className="grid gap-8">
-              <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                  <AvatarImage src="/avatars/01.png" alt="Avatar" />
-                  <AvatarFallback>OM</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                  <p className="text-sm font-medium leading-none">
-                    Olivia Martin
-                  </p>
-                  <p className="text-sm text-muted-foreground">
-                    olivia.martin@email.com
-                  </p>
-                </div>
-                <div className="ml-auto font-medium">+$1,999.00</div>
-              </div>
-              <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                  <AvatarImage src="/avatars/02.png" alt="Avatar" />
-                  <AvatarFallback>JL</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                  <p className="text-sm font-medium leading-none">
-                    Jackson Lee
-                  </p>
-                  <p className="text-sm text-muted-foreground">
-                    jackson.lee@email.com
-                  </p>
-                </div>
-                <div className="ml-auto font-medium">+$39.00</div>
-              </div>
-              <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                  <AvatarImage src="/avatars/03.png" alt="Avatar" />
-                  <AvatarFallback>IN</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                  <p className="text-sm font-medium leading-none">
-                    Isabella Nguyen
-                  </p>
-                  <p className="text-sm text-muted-foreground">
-                    isabella.nguyen@email.com
-                  </p>
-                </div>
-                <div className="ml-auto font-medium">+$299.00</div>
-              </div>
-              <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                  <AvatarImage src="/avatars/04.png" alt="Avatar" />
-                  <AvatarFallback>WK</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                  <p className="text-sm font-medium leading-none">
-                    William Kim
-                  </p>
-                  <p className="text-sm text-muted-foreground">
-                    will@email.com
-                  </p>
-                </div>
-                <div className="ml-auto font-medium">+$99.00</div>
-              </div>
-              <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                  <AvatarImage src="/avatars/05.png" alt="Avatar" />
-                  <AvatarFallback>SD</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                  <p className="text-sm font-medium leading-none">
-                    Sofia Davis
-                  </p>
-                  <p className="text-sm text-muted-foreground">
-                    sofia.davis@email.com
-                  </p>
-                </div>
-                <div className="ml-auto font-medium">+$39.00</div>
-              </div>
+              {renderWorkspaces}
             </CardContent>
           </Card>
         </div>
