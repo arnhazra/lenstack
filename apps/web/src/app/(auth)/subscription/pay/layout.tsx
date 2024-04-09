2f5974d1c50ae044557d19bb1401e5901b7bf16d
Arnab Hazra <arnhazra@gmail.com>
2024-04-09 10:36:40 +0530

    publish

--- apps/web/src/app/(auth)/subscription/pay/layout.tsx
+++ apps/web/src/app/(auth)/subscription/pay/layout.tsx
@@ -3,7 +3,7 @@ import Suspense from "@/components/suspense"
 import { GlobalContext } from "@/context/providers/globalstate.provider"
 import { ReactNode, useContext } from "react"
 import Link from "next/link"
-import { ArrowRightIcon } from "@radix-ui/react-icons"
+import { ArrowRight } from "lucide-react"
 
 export default function PayLayout({ children }: { children: ReactNode }) {
   const [{ userState }] = useContext(GlobalContext)
@@ -14,7 +14,7 @@ export default function PayLayout({ children }: { children: ReactNode }) {
         <div className="box">
           <p className="branding">Hold On</p>
           <p className="text-muted">Seems like you are already having an active subscription</p>
-          <Link className="btn btn-primary btn-block" href="/products">Go to Products Page<ArrowRightIcon className="icon-right" /></Link>
+          <Link className="btn btn-primary btn-block" href="/products">Go to Products Page<ArrowRight className="icon-right" /></Link>
         </div>
       </div>
     </Suspense>
