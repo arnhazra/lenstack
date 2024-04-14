2b0c0e9d0a496eaca1221b0f566c37e64b31b086
Arnab Hazra <arnhazra@gmail.com>
2024-04-14 12:19:30 +0530

    publish

--- apps/web/src/app/error.tsx
+++ apps/web/src/app/error.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect } from "react"
+import { Fragment, useEffect } from "react"
 import { useRouter } from "next/navigation"
 import { cn } from "@/lib/utils"
 
@@ -11,8 +11,8 @@ export default function Error() {
   }, [])
 
   return (
-    <div className="fixed inset-0 flex justify-center items-center z-50 bg-white">
-      <div>
+    <Fragment>
+      <div className="fixed inset-0 flex justify-center items-center z-50 bg-white">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="36"
@@ -23,11 +23,12 @@ export default function Error() {
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
-          className={cn("animate-spin")}
+          className={cn("animate-spin", "text-neutral-600")}
+          style={{ animation: "spin 1s linear infinite" }}
         >
           <path d="M21 12a9 9 0 1 1-6.219-8.56" />
         </svg>
       </div>
-    </div>
+    </Fragment>
   )
 }
--- apps/web/src/components/loading/index.tsx
+++ apps/web/src/components/loading/index.tsx
@@ -6,22 +6,21 @@ export default function Loading() {
   return (
     <Fragment>
       <div className="fixed inset-0 flex justify-center items-center z-50 bg-white">
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="36"
-            height="36"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className={cn("animate-spin")}
-          >
-            <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-          </svg>
-        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="36"
+          height="36"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className={cn("animate-spin", "text-neutral-600")}
+          style={{ animation: "spin 1s linear infinite" }}
+        >
+          <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+        </svg>
       </div>
     </Fragment>
   )
