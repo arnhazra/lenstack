9f311f197e74f2e04d934b9367e4048fc04689ca
Arnab Hazra <arnhazra@gmail.com>
2024-04-20 18:08:38 +0530

    publish

--- apps/web/src/app/(auth)/subscription/pay/page.tsx
+++ apps/web/src/app/(auth)/subscription/pay/page.tsx
@@ -1,18 +1,17 @@
 "use client"
 import { Button } from "@/components/ui/button"
 import { endPoints } from "@/constants/api-endpoints"
-import { loadStripe } from '@stripe/stripe-js'
 import axios from "axios"
 
 export default function page() {
   const checkout = async () => {
-    const stripe = await loadStripe("pk_test_51P5Q94SJwCI16KpcELsnIAvQbLJHVPPiIm5pmEaz0X8HUnzdgAIj82cbwqpq4oaK2aZyRd64XtNZ2vjFu9dfPn8E00LIOea6kT")
-    const body = {
-      products: "Subscription"
-    }
+    // const stripe = await loadStripe("pk_test_51P5Q94SJwCI16KpcELsnIAvQbLJHVPPiIm5pmEaz0X8HUnzdgAIj82cbwqpq4oaK2aZyRd64XtNZ2vjFu9dfPn8E00LIOea6kT")
+    // const body = {
+    //   products: "Subscription"
+    // }
 
-    const response = await axios.post(endPoints.createCheckoutSession, body)
-    const result = stripe?.redirectToCheckout({ sessionId: response.data.sessionId })
+    // const response = await axios.post(endPoints.createCheckoutSession, body)
+    // const result = stripe?.redirectToCheckout({ sessionId: response.data.sessionId })
   }
 
   return (
