6dfed86bf7a7620302fb8d983d8654a2fb1c9948
Arnab Hazra <arnhazra@gmail.com>
2024-04-21 13:29:47 +0530

    publish

--- apps/api/src/api/subscription/commands/delete-subscription.command.ts
+++ apps/api/src/api/subscription/commands/delete-subscription.command.ts
@@ -1,6 +1,6 @@
 import { SubscriptionModel } from "../models/subscription.model"
 
 export async function deleteSubscriptionCommand(userId: string) {
-  const subscription = await SubscriptionModel.findOneAndDelete({ userId })
-  return subscription
+  await SubscriptionModel.findOneAndDelete({ userId })
+  return true
 }
