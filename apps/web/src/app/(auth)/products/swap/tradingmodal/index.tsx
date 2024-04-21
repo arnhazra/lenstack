befac4d5e7cecb9a35abd5ead7e1d469a3cfb312
Arnab Hazra <arnhazra@gmail.com>
2024-04-21 16:24:22 +0530

    publish

--- apps/web/src/app/(auth)/products/swap/tradingmodal/index.tsx
+++ apps/web/src/app/(auth)/products/swap/tradingmodal/index.tsx
@@ -195,7 +195,7 @@ export function TradingModal({ token: { tokenContractAddress, tokenName, tokenSy
 
   return (
     <Dialog open={dialogVisible}>
-      <DialogTrigger>
+      <DialogTrigger asChild>
         <Button variant="outline" onClick={(): void => setDialogVisible(true)}>Trade</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
