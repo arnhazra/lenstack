5ce9325fb9c54a5379964b674815d9ccda07c6de
Arnab Hazra <arnhazra@gmail.com>
2024-07-21 21:15:09 +0530

    publish

--- apps/api/src/api/products/(products)/analytics/analytics.repository.ts
+++ apps/api/src/api/products/(products)/analytics/analytics.repository.ts
@@ -1,6 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
-import { InjectModel } from '@nestjs/mongoose'
-import { Model } from 'mongoose'
+import { Injectable } from '@nestjs/common'
 import { CreateAnalyticsDto } from "./dto/create-analytics.dto"
 import { AnalyticsModel } from "./schemas/analytics.schema"
 
