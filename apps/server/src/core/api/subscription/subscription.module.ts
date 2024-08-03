84053546c1390cca14dac534e3fae9745bc1bda9
Arnab Hazra <arnhazra@gmail.com>
2024-08-03 11:47:43 +0530

    publish

--- apps/server/src/core/api/subscription/subscription.module.ts
+++ apps/server/src/core/api/subscription/subscription.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common"
 import { SubscriptionService } from "./subscription.service"
 import { SubscriptionController } from "./subscription.controller"
-import { HttpModule } from "@nestjs/axios"
 
 @Module({
-  imports: [HttpModule],
   controllers: [SubscriptionController],
   providers: [SubscriptionService],
 })
