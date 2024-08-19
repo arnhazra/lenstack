4794a44461066af832f2700c005baf427619076d
Arnab Hazra <arnhazra@gmail.com>
2024-08-19 10:41:44 +0530

    publish

--- apps/client/src/components/loaderIcon/index.tsx
+++ apps/client/src/components/loaderIcon/index.tsx
@@ -1,13 +1,13 @@
 import { cn } from "@/lib/utils"
-import { LoaderCircle } from "lucide-react"
+import { Loader } from "lucide-react"
 
 export default function LoaderIcon() {
   return (
-    <LoaderCircle
+    <Loader
       width="16"
       height="16"
       className={cn("animate-spin", "text-white", "me-2")}
-      style={{ animation: "spin 1s linear infinite" }}
+      style={{ animation: "spin 1.5s linear infinite" }}
     />
   )
 }
