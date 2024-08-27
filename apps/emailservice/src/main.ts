ee8f6536bd082f6ffe0b3c4ea5a5066d29006fd1
Arnab Hazra <arnabhazra@Arnabs-MacBook.local>
2024-08-27 21:26:24 +0530

    publish

---
+++ apps/emailservice/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
+import { envConfig } from "env.config";
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [envConfig.rabbitMqURI],
+      queue: "email_queue",
+      queueOptions: {
+        durable: false
+      },
+    },
+  })
+
+  app.listen()
+}
+
+bootstrap();
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,29 +2,13 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import { INestApplication, ValidationPipe } from "@nestjs/common"
 import { envConfig } from "./env.config"
-import { MicroserviceOptions, Transport } from "@nestjs/microservices"
-import { QueueUnion } from "./microservices/events.union"
-import { EmailModule } from "./microservices/email/email.module"
 
 async function bootstrap(): Promise<void> {
   const app: INestApplication<any> = await NestFactory.create(AppModule)
   app.setGlobalPrefix("api")
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({ exposedHeaders: ["token"] })
-
-  const emailMicroservice = await NestFactory.createMicroservice<MicroserviceOptions>(EmailModule, {
-    transport: Transport.RMQ,
-    options: {
-      urls: [envConfig.rabbitMqURI],
-      queue: QueueUnion.EmailQueue,
-      queueOptions: {
-        durable: false
-      },
-    },
-  })
-
   await app.listen(envConfig.apiPort)
-  await emailMicroservice.listen()
 }
 
 bootstrap()
