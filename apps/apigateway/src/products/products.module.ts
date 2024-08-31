2d1145e438bed703e7b9ac885cbf1bf320c13b7b
arnab <arnhazra@gmail.com>
2024-08-31 16:50:53 +0530

    publish

--- apps/apigateway/src/products/products.module.ts
+++ apps/apigateway/src/products/products.module.ts
@@ -1,11 +1,25 @@
-import { Module } from "@nestjs/common"
+import { Module, DynamicModule } from "@nestjs/common"
 import { CopilotModule } from "./copilot/copilot.module"
 import { DatamarketplaceModule } from "./datamarketplace/datamarketplace.module"
 import { HttpnosqlModule } from "./httpnosql/httpnosql.module"
 import { WebAnalyticsModule } from "./webanalytics/webanalytics.module"
 import { IdentityModule } from "./identity/identity.module"
-import { DatabaseModule } from "./database.module"
 import { DbConnectionMap } from "src/utils/db-connection.map"
+import { MongooseModule } from "@nestjs/mongoose";
+import { envConfig } from "src/env.config";
+
+@Module({})
+export class DatabaseModule {
+  static forChild(mappedDb: DbConnectionMap): DynamicModule {
+    return MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: envConfig.productsDatabaseURI,
+        dbName: mappedDb,
+      }),
+      connectionName: mappedDb,
+    });
+  }
+}
 
 @Module({
   imports: [
