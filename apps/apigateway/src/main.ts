8a1dd17a72605d9bb3730d9ebeb56dce3f5beec0
arnab <arnhazra@gmail.com>
2024-09-12 20:56:41 +0530

    publish

--- apps/apigateway/src/main.ts
+++ apps/apigateway/src/main.ts
@@ -2,12 +2,9 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import { INestApplication, ValidationPipe } from "@nestjs/common"
 import { envConfig } from "./env.config"
-import { Request, Response } from "express"
-import { otherConstants } from "./shared/utils/constants/other-constants"
 
 async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(AppModule)
-  app.use("/", (req: Request, res: Response) => res.redirect(otherConstants.tokenIssuer))
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({ exposedHeaders: ["token"] })
   await app.listen(envConfig.apiPort)
