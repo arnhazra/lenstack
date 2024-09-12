4be70396e66ab1f08b385b59be4bd64b5273e316
arnab <arnhazra@gmail.com>
2024-09-12 20:29:06 +0530

    publish

--- apps/uiservice/src/context/providers/index.tsx
+++ apps/uiservice/src/context/providers/index.tsx
@@ -4,22 +4,19 @@ import ReactQueryProvider from "./reactquery.provider"
 import { GlobalStateProvider } from "./globalstate.provider"
 import { ConfirmProvider } from "./confirm.provider"
 import { PromptProvider } from "./prompt.provider"
-import { CachebustingProvider } from "./cachebusting.provider"
 import { Toaster } from "@/components/ui/toaster"
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <CachebustingProvider>
-      <ReactQueryProvider>
-        <GlobalStateProvider>
-          <ConfirmProvider>
-            <PromptProvider>
-              {children}
-            </PromptProvider>
-          </ConfirmProvider>
-          <Toaster />
-        </GlobalStateProvider>
-      </ReactQueryProvider>
-    </CachebustingProvider>
+    <ReactQueryProvider>
+      <GlobalStateProvider>
+        <ConfirmProvider>
+          <PromptProvider>
+            {children}
+          </PromptProvider>
+        </ConfirmProvider>
+        <Toaster />
+      </GlobalStateProvider>
+    </ReactQueryProvider>
   )
 }
