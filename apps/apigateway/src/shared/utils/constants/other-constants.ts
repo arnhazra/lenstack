20d8082a35fe890d950865c484f14536d0e68f3e
arnab <arnhazra@gmail.com>
2024-10-21 07:44:19 +0530

    publish

--- apps/apigateway/src/shared/utils/constants/other-constants.ts
+++ apps/apigateway/src/shared/utils/constants/other-constants.ts
@@ -1,12 +1,12 @@
 import { envConfig } from "src/env.config"
 
-export const devUri = "http://localhost:3000"
-export const prodUri = `https://${envConfig.brandName.toLowerCase()}.vercel.app`
+export const devUIUri = "http://localhost:3000"
+export const prodUIUri = `https://${envConfig.brandName.toLowerCase()}.vercel.app`
 
 export const otherConstants = {
-  tokenIssuer: prodUri,
-  stripeRedirectUriDev: `${devUri}/dashboard`,
-  stripeRedirectUriProd: `${prodUri}/dashboard`,
+  tokenIssuer: devUIUri,
+  stripeRedirectUriDev: `${devUIUri}/dashboard`,
+  stripeRedirectUriProd: `${prodUIUri}/dashboard`,
   stripeConfigBaseUriDev: "http://localhost:8000/pricing",
   stripeConfigBaseUriProd: `https://api-${envConfig.brandName.toLowerCase()}.vercel.app/pricing`
 }
