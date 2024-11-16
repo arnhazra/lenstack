6c93ccbdac646d9dad179826725e339a84d9fa25
arnab <arnhazra@gmail.com>
2024-11-16 09:55:33 +0530

    publish

--- apps/uiservice/src/app/(platform)/settings/about/page.tsx
+++ apps/uiservice/src/app/(platform)/settings/about/page.tsx
@@ -1,20 +1,13 @@
-import { brandName } from "@/shared/constants/global-constants"
-import Link from "next/link"
+import { brandName, uiConstants } from "@/shared/constants/global-constants"
+import SectionPanel from "../(components)/sectionpanel"
+import { InfoIcon } from "lucide-react"
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="bg-slate-200 w-24 h-24 rounded-2xl flex items-center justify-center ecosystem">
-        <span className="text-6xl text-white font-bold">17</span>
-      </div>
-      <p className="text-xs text-slate-700 mt-4">{brandName} EcoSystem 17.1</p>
-      <Link
-        target="_blank"
-        className="text-xs text-blue-500"
-        href="https://github.com/arnhazra/arcstack/blob/main/CHANGELOG.md"
-      >
-        View Changelog
-      </Link>
-    </div>
+    <SectionPanel
+      icon={<InfoIcon className="scale-75" />}
+      title={`${brandName} Ecosystem Version`}
+      content="17.2"
+    />
   )
 }
