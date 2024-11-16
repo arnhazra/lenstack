e968fd7ed164bef48f61278f11b4be5b178dfb75
arnab <arnhazra@gmail.com>
2024-11-16 15:40:32 +0530

    publish

--- apps/uiservice/src/app/layout.tsx
+++ apps/uiservice/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { brandName } from "@/shared/constants/global-constants"
 import { Quicksand } from "next/font/google"
 import Providers from "@/shared/providers"
 import NextTopLoader from "nextjs-toploader"
-import "@/shared/styles/globals.sass"
+import "../shared/styles/globals.sass"
 
 const quickSand = Quicksand({ subsets: ["latin"], weight: ["700"] })
 
@@ -23,7 +23,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         />
       </head>
       <body className={quickSand.className}>
-        <NextTopLoader color="#334155" showSpinner={false} speed={300} />
+        <NextTopLoader color="#475569" showSpinner={false} speed={300} />
         <Providers>
           <main className="min-h-screen w-full bg-slate-50">{children}</main>
         </Providers>
